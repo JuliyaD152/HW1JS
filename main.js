61ea74d91b052600cbb18f3f5fa0c4991f4df8ba
JuliyaD152 <60539550+JuliyaD152@users.noreply.github.com>
2020-06-15 21:37:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+console.log('Sample JavaScript HW#1');
+
+var myNum = 10;
+var myStr = 'строка';
+var myBool = true;
+var myArr = [1, 2, 3, 4, 5];
+
+var myObj = {
+    first: 'First Name',
+    last: 'Last Name'};
+
+
+var decimal2;
+ decimal2 = myNum.toFixed(2); 
+
+var i = 5;
+    console.log (i++);
+    console.log (++i);
+    console.log (i--);
+    console.log (--i);
+
+ var myTest = 20;
+        console.log(myTest += myNum);
+        console.log(myTest -= myNum);
+        console.log(myTest *= myNum);
+        console.log(myTest /= myNum);
+        console.log(myTest %= 6);
+
+ var myPi = Math.PI;
+    console.log (myPi);
+ var myRound = Math.round(89.279);
+    console.log (myRound);
+var myRandom = Math.random()*10;
+    console.log(myRandom);
+var myPow = Math.pow (3,5);
+    console.log (myPow);
+
+var strObj = {
+    str: "Мама мыла раму, рама мыла маму",
+};
+    strObj.length = strObj.str.length;
+    console.log(strObj.length);
+
+var isRamaPos = strObj.str.indexOf('рама');
+    console.log(isRamaPos);
+
+var strReplace = strObj.str.replace('Мама мыла раму, рама мыла маму','Мама моет раму, Рама держит маму');
+    console.log(strReplace);
+    console.log(strReplace.toUpperCase());
+    console.log(strReplace.toLowerCase());
+    
+
